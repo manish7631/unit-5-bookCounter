4e49f4fa0246e62130426e3cc830c3f3b389648b
manishfw13 <manish_fw13_347@masai.school>
2022-04-10 18:19:24 +0530

    first commit

---
+++ src/components/Inventory.jsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+
+export const Inventory = () => {
+  const [inv, setInv] = useState({
+    books: 10,
+    notebooks: 13,
+    pens: 40,
+    inkpens:24,
+  });
+   
+    // Create add and remove functions here that changes the
+    const books= inv.books
+  const notebooks=inv.notebooks
+  const pens=inv.pens
+  const inkpens=inv.inkpens
+    // state.
+    
+const Change=(value,title)=>{
+  if(title==="book"){
+    if(books<=0&&value===-1)
+    {
+      return 
+    }else{
+      setInv(prevState =>{
+        return {...prevState,books:prevState.books+value}
+      })
+    }
+    
+  }else if(title==="notebook")
+  {
+    if(notebooks<=0&&value===-1)
+    {
+      return 
+    }else{
+      setInv(prevState =>{
+        return {...prevState,notebooks:prevState.notebooks+value}
+      })
+    }
+    
+  }else if(title==="pen")
+  {
+    if(pens<=0&&value===-1)
+    {
+      return 
+    }else{
+      setInv(prevState =>{
+        return {...prevState,pens:prevState.pens+value}
+      })
+    }
+    
+   
+  }else if(title==="inkpen")
+  {
+    if(inkpens<=0&&value===-1)
+    {
+      return 
+    }else{
+      setInv(prevState =>{
+        return {...prevState,inkpens:prevState.inkpens+value}
+      })
+    }
+   
+  }
+ 
+  
+}
+let total=books+notebooks+pens+inkpens
+
+
+
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        borderRadius: "3px",
+        padding: "10px",
+        display: "flex",
+        flexDirection: "column",
+        width: "400px",
+      }}
+    >
+      <div className="items">
+        <span>Books: </span>
+        <button className="circlularButton" onClick={()=>{
+          Change(1,"book")
+        }}>+</button>
+        <button className="circlularButton"  onClick={()=>{
+        Change(-1,"book")
+        }}>-</button>
+        <span>{books}</span>
+      </div>
+      <div className="items">
+        <span>Notebooks: </span>
+        <button className="circlularButton" onClick={()=>{
+          Change(1,"notebook")
+        }} >+</button>
+        <button className="circlularButton"  onClick={()=>{
+          Change(-1,"notebook")
+        }}>-</button>
+        <span>{notebooks}</span>
+      </div>
+      <div className="items">
+        <span>Pen: </span>
+        <button className="circlularButton" onClick={()=>{
+          Change(1,"pen")
+        }} >+</button>
+        <button className="circlularButton" onClick={()=>{
+          Change(-1,"pen")
+        }} >-</button>
+        <span>{pens}</span>
+      </div>
+      <div className="items">
+        <span>Ink Pens: </span>
+        <button className="circlularButton" onClick={()=>{
+          Change(1,"inkpen")
+        }} >+</button>
+        <button className="circlularButton" onClick={()=>{
+          Change(-1,"inkpen")
+        }} >-</button>
+        <span>{inkpens}</span>
+      </div>
+            {/*calculate total and show it*/}
+      total: {total}
+    </div>
+  );
+};
